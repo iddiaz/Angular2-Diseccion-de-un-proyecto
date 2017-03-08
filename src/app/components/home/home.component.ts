1856c04fd1863c90bfee19495850d576cbef4513
iddiaz <diazdiazivan@gmail.com>
2017-03-08 14:53:10 +0100

    Organizacion de estructura

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <h2>Organizar proyectos con Angular 2</h2>
+    <p>
+      home Works!
+    </p>
+  `,
+  styles: []
+})
+export class HomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
